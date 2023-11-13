edcd802c70f8147f2451cc59d79798e8861f3634
zhyk <zhyk@list.ru>
2023-11-13 20:50:02 +0300

    Задача № 42

--- Task39/Task42/Program.cs
+++ Task39/Task42/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+// 45 -> 101101
+// 3  -> 11
+// 2  -> 10
+
+int side(string msg)
+{
+    System.Console.WriteLine(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void GetBinaryView(int number)
+{
+    if (number == 0)
+    {
+        return;
+    }
+    GetBinaryView(number / 2);
+    System.Console.Write(number % 2);
+}
+
+int number = side("Введите число: ");
+GetBinaryView(number);
+
+
